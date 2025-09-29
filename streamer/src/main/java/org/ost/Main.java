afb404e0c7c34366b3c14545e0d0aa0351971c68
houmam <houmam.alb@movo.delivery>
2025-09-29 19:42:27 +0200

    Clean up

--- streamer/src/main/java/org/ost/Main.java
+++ streamer/src/main/java/org/ost/Main.java
@@ -1,7 +1,4 @@
 package org.ost;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.clients.admin.*;
@@ -16,8 +13,7 @@ import java.io.InputStream;
 public class Main {
     static String topic = "sensor-data";
     public static void main(String[] args) {
-        int skipLines = 3000; // Number of lines to skip (e.g., header)
-        // Read bootstrap servers from environment variable, fallback to localhost:9092
+        int skipLines = 3000; // Number of lines to skip
         final String bootstrapServers = System.getenv().getOrDefault("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092");
         Properties props = new Properties() {{
             put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -25,20 +21,6 @@ public class Main {
             put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
             put(ProducerConfig.ACKS_CONFIG, "1");
         }};
-
-//        Producer<String, String> producer = new KafkaProducer<>(props);
-
-
-        final String key = "key1";
-        final String value = "value1";
-        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, value);
-
-//        producer.send(record);
-//        producer.flush();
-//        producer.close();
-        System.out.println("Message sent to topic '" + topic + "' with key '" + key + "' and value '" + value + "'.");
-
-        // Uncomment below to stream CSV data to Kafka
 
         try (KafkaProducer<String, String> producer = new KafkaProducer<>(props)) {
             // Read CSV from resources
